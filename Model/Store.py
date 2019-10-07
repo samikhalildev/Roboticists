fbc7d7ddc2a9f839712780aed4647bbeddfff8bb
samikhalildev <sami.khalil.dev@gmail.com>
2019-10-07 16:54:48 +1100

    Extend store to include users

--- Model/Store.py
+++ Model/Store.py
@@ -1,7 +1,10 @@
+import json, os
 class Store:
     
     def __init__(self):
 
+        self.users_path = os.getcwd() + '/Model/data/users.json'
+        self.knownUsers = self.loadJSON()
         self.products = [
             {
                 "productName":"Mars Pods",
@@ -30,7 +33,31 @@ class Store:
 
         self.availableItems = ['chocolate', 'beverages', 'fruit', 'beverages', 'pods', 'mars', 'mars pods', 'apple', 'pepsi', 'cola', 'soft drink', 'drink']
 
+    def loadJSON(self):
+        with open(self.users_path, 'r') as f:
+            try:
+                return json.load(f)
+            except IOError as err:
+                print('err: {0}'.format(err))
+                return False
+
+    def saveUser(self, user, features):
+        if os.path.exists(self.users_path):
+            with open(self.users_path, 'w') as f:
+                try:
+                    self.knownUsers[user] = features
+                    json.dump(self.knownUsers, f, ensure_ascii=False)
+                    return True
+                except:
+                    return False
+
     def getItem(self, query):
         for item in self.products:
             if query in item['similarItems']:
                 return item
+
+
+if __name__ == "__main__":
+    store = Store()
+    store.saveUser('test', '3123')
+    print store.knownUsers
