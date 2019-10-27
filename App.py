36f7011c73e20adce3d73d8b77fda0b9951f256b
Sami <sami.khalil.dev@gmail.com>
2019-10-27 13:02:24 +1100

    Update App.py

--- App.py
+++ App.py
@@ -3,9 +3,9 @@ from Model.Store import Store
 from Model.Yolo_Model import Model
 import os
 
-#try:
-robot = Robot(os.getcwd(), Store, Model)
-robot.run()
+try:
+    robot = Robot(os.getcwd(), Store, Model)
+    robot.run()
     
-#except (RuntimeError, TypeError, NameError, ValueError) as err:
-    #print('ERROR ---> {0} <---'.format(err))
+except (RuntimeError, TypeError, NameError, ValueError) as err:
+    print('ERROR ---> {0} <---'.format(err))
